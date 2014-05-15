ed8a315f7c6270431a81f955f639aac4acd0a677
tanlivia <hlh.hust@gmail.com>
2014-05-15 10:14:09 -0700

    Add comment for sign up page to create new user.
    This will be able to create a new user instead of admin. Default comment out since we only need one username & password for lawyer.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Blog::Application.routes.draw do
   get "admin" => "sessions#new", :as => "admin"
   get "log_out" => "sessions#destroy", :as => "log_out"
 
+  # If you want to create a new username and password, please uncommend the following two lines out.
   # get "sign_up" => "users#new", :as => "sign_up"
-
   # resources :users
   resources :sessions
 
